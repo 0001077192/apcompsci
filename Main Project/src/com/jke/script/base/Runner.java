8524ffe1a66d49636a4e04a900095d7aebd75aa6
Jeremy E. <0001077192@ccisd.net>
2018-04-04 13:32:48 -0500

    Added simple test to Runner

--- Main Project/src/com/jke/script/base/Runner.java
+++ Main Project/src/com/jke/script/base/Runner.java
@@ -4,7 +4,6 @@ public class Runner
 {
 	public static void main( String[] args )
 	{
-		System.out.println( "Hello World!" );
-		System.out.println( new SyntaxErrorException( "script.txt", "n / 0", 1, "/ 0", "Divide by zero" ) );
+		
 	}
 }
